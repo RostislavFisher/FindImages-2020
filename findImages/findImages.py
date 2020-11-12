6d7bf8e9780b279b14f7faa7ead84049efe8dffd
RostislavFisher <59475110+RostislavFisher@users.noreply.github.com>
2020-11-12 19:01:44 +0200

    Add files via upload

--- findImages/findImages.py
+++ findImages/findImages.py
@@ -3,35 +3,44 @@ import bs4
 import random
 
 
-def parseImgur(amountOfIterations=9999, fileExpansion='.png'):
+def parseImgur(argScanner):
+    amountOfIterations = argScanner.amountOfIteration
+    fileExpansion = argScanner.fileExpansion
+    numberInFileName = len(open("List.txt", 'r').readlines())
+
     Key = '123456789qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM'
     url = 'https://i.imgur.com/'
     removedURL = "https://i.imgur.com/removed.png"
     RemovedReq = requests.get(removedURL, allow_redirects=True).content
-    numOfIteration = len(open("List.txt", 'r').readlines())
-    while amountOfIterations < numOfIteration:
+    numOfIteration = 0
+
+    while amountOfIterations > numOfIteration:
         Adr = url + ''.join([random.choice(list(Key)) for x in range(7)]) + fileExpansion
         RequestedImage = requests.get(Adr, allow_redirects=True).content
         if RequestedImage != RemovedReq:
             ListWrite = open("List.txt", 'a')
             print(Adr)
-            open(str(numOfIteration) + fileExpansion, 'wb').write(RequestedImage)
+            open("{0}/{1}{2}".format(argScanner.imageDir, str(numberInFileName), fileExpansion), 'wb').write(
+                RequestedImage)
             ListWrite.write(Adr + "\n")
             numOfIteration += 1
+            numberInFileName += 1
             ListWrite.close()
 
 
-def parseLightShot(amountOfIterations=9999, fileExpansion='.png'):
-    import bs4
+def parseLightShot(argScanner):
+    amountOfIterations = argScanner.amountOfIteration
+    fileExpansion = argScanner.fileExpansion
+
     Key = '123456789qwertyuiopasdfghjklzxcvbnm'
     url = 'https://prnt.sc/'
-    FileLen = len(open("List.txt", 'r').readlines())
+    numberInFileName = len(open("List.txt", 'r').readlines())
     removedURL = "https://prnt.sc/18jgksdgh"
     RemovedReq = requests.get(removedURL, headers={'User-Agent': 'Mozilla/5.0'})
     RemovedReq = bs4.BeautifulSoup(RemovedReq.text, "html.parser")
     RemovedReq = RemovedReq.find('img', id='screenshot-image')['src']
-    numOfIteration = 0
 
+    numOfIteration = 0
     while amountOfIterations > numOfIteration:
         Adr = url + ''.join([random.choice(list(Key)) for x in range(6)])
         RequestedImage = requests.get(Adr, headers={'User-Agent': 'Mozilla/5.0'})
@@ -41,8 +50,8 @@ def parseLightShot(amountOfIterations=9999, fileExpansion='.png'):
         if RequestedImage != RemovedReq:
             ListWrite = open("List.txt", 'a')
             ListWrite.write(Adr + "\n")
-            numOfIteration += 1
             ListWrite.close()
             RequestedImage = requests.get(RequestedImage, allow_redirects=True).content
-            open(str(FileLen) + fileExpansion, 'wb').write(RequestedImage)
-            FileLen = FileLen + 1
+            open("{0}/{1}{2}".format(argScanner.imageDir, str(numberInFileName), fileExpansion), 'wb').write(RequestedImage)
+            numberInFileName += 1
+            numOfIteration += 1
--- main.py
+++ main.py
@@ -1,19 +1,54 @@
 import argparse
 from findImages.findImages import parseLightShot, parseImgur
+import os
+
+baseImageDir = "images"
 
 parseList = {
-    '1': parseLightShot,
-    '2': parseImgur,
+    '1': parseImgur,
+    '2': parseLightShot,
 }
 
 
 class argumentScanner:
+    """
+    argumentScanner - класс, использованый для удобнейшого способа хранить информацию из аргументов.
+
+    - self.command хранит полный список(если быть точнее - словарь) аргументов
+
+    - self.siteToParse - атрибут, хранящий ID сайта, которого мы будем парсить (переадрессация стоит в parseList) (хранится в int)
+
+    - self.amountOfIterations - атрибут, хранящий количество фотографий, сколько мы будем сохранять (хранится в int)
+
+    - self.fileExpansion - атрибут, хранящий расширение файла, которое будем искать
+
+    - self.imageDir - атрибут, хранящий путь к папке с файлами
+
+    Запуск программы из cmd:
+
+    - Типичный запуск:
+        $ python main.py
+    - Запуск парсинга конкретного сервиса:
+        $ python main.py --siteToParse 2
+    - Запуск парсинга с фиксированным количеством файла:
+        $ python main.py --amountOfIterations
+    - Запуск парсинга с фикированным расширеним:
+        $ python main.py --fileExpansion .png
+
+    - Запуск парсинга и сохранения файлов в конкретную папку:
+        $ python main.py --imageDir images
+
+    """
     def __init__(self, **kwargs):
         self.command = kwargs.get("command")
         self.siteToParse = self.command["siteToPars"] if self.command["siteToPars"] is not None else '1'
-        self.amountOfIteration = self.command["amountOfIterations"] if self.command[
+        self.amountOfIteration = int(self.command["amountOfIterations"]) if self.command[
                                                                            "amountOfIterations"] is not None else 9999
         self.fileExpansion = self.command["fileExpansion"] if self.command["fileExpansion"] is not None else '.png'
+        self.imageDir = self.command["baseDir"] if self.command["baseDir"] is not None else baseImageDir
+
+        if not os.path.exists(self.imageDir):
+            os.makedirs(self.imageDir)
 
 
 if __name__ == '__main__':
@@ -21,5 +56,6 @@ if __name__ == '__main__':
     parser.add_argument('-stp', '--siteToPars')
     parser.add_argument('-amofi', '--amountOfIterations')
     parser.add_argument('-fexp', '--fileExpansion')
+    parser.add_argument('-bd', '--baseDir')
     argScanner = argumentScanner(command=vars(parser.parse_args()))
-    parseList[argScanner.siteToParse](argScanner.amountOfIteration, argScanner.fileExpansion)
+    parseList[argScanner.siteToParse](argScanner)
